2f87af9cd0cbcd097d7e5fbbc1e729f424d75f00
Nidhikashesh <nidhikashesh008@gmail.com>
2026-04-27 13:33:48 +0530

    implement job search functionality with Search fillter in dashboard

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -1,9 +1,26 @@
-var posts=require("../models/jobPostModel");
+var jobPosts=require("../models/jobPostModel");
 exports.authenticate= async function(req, res, next) {
    if(!req.session.user){
     return res.redirect("/");
    } 
-   const Posts= await posts.find().sort({createdAt:-1});
-   res.render("dashboard",{user:req.session.user,Posts})
    
+   try {
+        const search = req.query.search || "";
+
+        const query = {
+            $or: [
+                { companyName: { $regex: search, $options: "i" } },
+                { jobRole: { $regex: search, $options: "i" } }
+            ]
+        };
+
+        const Posts = await jobPosts.find(query).sort({ createdAt: -1 });
+
+        res.render("dashboard",{user:req.session.user,Posts,search});
+
+    } catch (err) {
+        console.log(err);
+
+        res.send("Error loading jobs");
+    }
 }
